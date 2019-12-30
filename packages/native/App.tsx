fbe6d53967f8bd75009d6582e59cc809b24ad7a0
Milos Dzepina <milos.dzepina@ezderm.com>
2019-12-30 10:50:36 +0100

    Fix vscode auto import issue - add main field to common's package.json

--- packages/native/App.tsx
+++ packages/native/App.tsx
@@ -4,13 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 import someFn from "common/someFn";
 import fn2 from "common/sub/fn2";
 
-
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <Text>{someFn(10,10)}</Text>
-  <Text>{fn2('there !')}</Text>
+      <Text>{fn2('there !')} </Text>
     </View>
   );
 }
